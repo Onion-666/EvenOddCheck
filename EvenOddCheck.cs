26d09a1a017d73b56e4b3fb75014a4607c788eff
Халяпин Вадим <vadim.halyapin@yandex.ru>
2020-09-22 12:21:30 +0300

    Добавьте файлы проекта.

---
+++ EvenOddCheck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+class EvenOddCheck
+{
+    static void Main()
+    {
+        int number, remainder;
+        number = Int32.Parse(Interaction.InputBox("Введите целое число",
+            "Проверка четности/нечетности"));
+        remainder = number % 2;
+        MessageBox.Show("Вы ввели " + (remainder == 0 ? "четное число" : "нечетное число"),
+            "Проверка четности/нечетности");
+    }
+}
